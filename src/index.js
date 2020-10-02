059f5b57ef5cf5142fed902aed090b34231353a6
AlekRing <okolcov60@gmail.com>
2020-10-02 16:18:24 +0300

    init: start arrays task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array == ['']) return 0
+  let min = Math.min(...array)
+
+  return min
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array == ['']) return 0
+  let max = Math.max(...array)
+
+  return max
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array == ['']) return 0
+
+  let sum = 0
+  let aver = 0
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i]
+  }
+  aver = sum / array.length
+
+  return aver
 }
